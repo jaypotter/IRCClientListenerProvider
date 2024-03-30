1d875ef0dfb8b47c03fe12a71c6ebc497a461a9e
Jay Potter <j@ypotter.ca>
2024-03-30 19:42:11 -0400

    Create IRCClientListenerProvider.php

---
+++ src/Potter/Socket/IRC/Client/IRCClientListenerProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter\Socket\IRC\Client;
+
+final class IRCClientListenerProvider extends AbstractListenerProvider
+{
+    public function getListenersForEvent(object $event): iterable
+    {
+        
+    }
+}
