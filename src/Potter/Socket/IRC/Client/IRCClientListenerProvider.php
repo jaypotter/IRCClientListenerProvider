c3b08ecb37218206a2f2ceb4d7e6d7cf7feaaf3a
Jay Potter <j@ypotter.ca>
2024-03-31 12:52:29 -0400

    Update IRCClientListenerProvider.php

--- src/Potter/Socket/IRC/Client/IRCClientListenerProvider.php
+++ src/Potter/Socket/IRC/Client/IRCClientListenerProvider.php
@@ -6,15 +6,29 @@ namespace Potter\Socket\IRC\Client;
 
 use \Potter\Event\Listener\AbstractListenerProvider;
 
+use Potter\Event\EventInterface;
+
 final class IRCClientListenerProvider extends AbstractListenerProvider
 {
     public function getListenersForEvent(object $event): iterable
     {
-        
+        if (!($event instanceof EventInterface)) {
+            return [];
+        }
+        switch ($event->getId()) {
+            case 'onConnection':
+                return function (EventInterface $event) {
+                    $event->getEmitter()->writeSocket('PASS abcdefghijk');
+                };
+        }
     }
     
     public function hasListenersForEvent(object $event): bool
     {
+        if (!($event instanceof EventInterface)) {
+            return false;
+        }
         
+        return true;
     }
 }
