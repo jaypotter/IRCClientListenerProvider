224c93c3c7d63197139a8ddbd547f96926e1bcfc
Jay Potter <j@ypotter.ca>
2024-04-01 17:11:12 -0400

    move logic to IRCClient

--- src/Potter/Resource/IRC/IRCClientListenerProvider.php
+++ src/Potter/Resource/IRC/IRCClientListenerProvider.php
@@ -8,9 +8,7 @@ use \Potter\Event\Listener\AbstractListenerProvider;
 use \Potter\Event\{EventInterface, Event};
 
 final class IRCClientListenerProvider extends AbstractListenerProvider
-{
-    private string $pingToken;
-    
+{    
     public function getListenersForEvent(object $event): iterable
     {
         if (!($event instanceof EventInterface)) {
@@ -19,25 +17,22 @@ final class IRCClientListenerProvider extends AbstractListenerProvider
         switch ($event->getId()) {
             case 'onConnection':
                 return [function (EventInterface $event) {
-                    $emitter = $event->getEmitter();
-                    $emitter->sendPassword();
-                    $emitter->sendNickname();
-                    $emitter->sendUsername();
+                    $event->getEmitter()->handleConnection();
                 }];
                 break;
             case 'onReceive':
                 return [function (EventInterface $event) {
-                    $this->onReceive($event);
+                    $this->handleMessage();
                 }];
                 break;
             case 'onPing':
                 return [function (EventInterface $event) {
-                    $event->getEmitter()->pong($this->pingToken);
+                    $event->getEmitter()->handlePing();
                 }];
                 break;
             case 'onPrivateMessage':
                 return [function (EventInterface $event) {
-                    echo 'RECEIVED PRIVATE MESSAGE FROM ' . $event->getEmitter()->getLastPrivateMessageSender() . PHP_EOL;
+                    $event->getEmitter()->handlePrivateMessage();
                 }];
                 break;
         }
@@ -50,29 +45,5 @@ final class IRCClientListenerProvider extends AbstractListenerProvider
         }
         
         return true;
-    }
-    
-    private function onReceive(EventInterface $event): void
-    {
-        $emitter = $event->getEmitter();
-        $message = $emitter->getLastMessage();
-        if (!str_contains($message, ' :')) {
-            return;
-        }
-        $split = explode(' :', $message, 2);
-        $left = $split[0];
-        $right = $split[1];
-        $eventDispatcher = $emitter->getEventDispatcher();
-        if ($left === "PING") {
-            $this->pingToken = $right;
-            $eventDispatcher->dispatch(new Event('onPing', $emitter));
-            return;
-        }
-        $leftSide = explode(' ', $left);
-        if ($leftSide[1] === "PRIVMSG") {
-            $sender = substr($leftSide[0], 1, strpos($leftSide[0], '!') - 2);
-            $emitter->receivePrivateMessage($sender, $right);
-            $eventDispatcher->dispatch(new Event('onPrivateMessage', $emitter));
-        }
     }
 }
